59935c60a27be0bcefcbc6cc185a05eaf1ef759c
BruceSerede <brucemuleri@gmail.com>
2024-05-22 15:44:17 +0300

    done

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
 class Dog:
-    # Class body goes here
+    def bark(self):
+        print("Woof!")
+    def sit(self):
+        print("The dog is sitting.")
+snoopy=Dog()
+snoopy.bark()
 
-    #Instance method definition
-    pass
+fido=Dog()
+fido.sit()
+
--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
 class Person:
-    # Class body goes here
+    def talk(self):
+        print("Hello World!")
+        
+    def walk(self):
+        print("The person is walking.")
 
-    #Instance method definition
-    pass
+John=Person()
+John.talk()
+
+Lee=Person()
+Lee.walk()
